29fee42edfe054785d9f80aa63df17532ea58f66
ZengJinCheng <2357000832@qq.com>
2025-04-14 22:07:06 +0800

    1

--- Companion/main.go
+++ Companion/main.go
@@ -13,9 +13,9 @@ import (
 	"syscall"
 	"text/template"
 
-	"github.com/AP-Hunt/FicsitRemoteMonitoringCompanion/Companion/exporter"
-	"github.com/AP-Hunt/FicsitRemoteMonitoringCompanion/Companion/prometheus"
-	"github.com/AP-Hunt/FicsitRemoteMonitoringCompanion/Companion/realtime_map"
+	"github.com/ZengJinCheng/FicsitRemoteMonitoringCompanion/Companion/exporter"
+	"github.com/ZengJinCheng/FicsitRemoteMonitoringCompanion/Companion/prometheus"
+	"github.com/ZengJinCheng/FicsitRemoteMonitoringCompanion/Companion/realtime_map"
 )
 
 var Version = "0.0.0-dev"
